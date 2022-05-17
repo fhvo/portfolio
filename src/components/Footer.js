eae4de037c9b025782739f711bfb7eb6f289a140
Brendon H <madeinthailand@me.com>
2022-05-17 21:27:03 +0700

    Update footer
    Update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,12 @@
 import gitLogo from '../logo/github.png'
 function Footer(){
     return (
-        <footer>
-            <p>© 2022 fhvo team</p>
-            <img src ={gitLogo} alt="gitLogo"/>
+        <footer >
+            <div className='container'>
+            <div className='left'>© 2022 fhvo team</div>
+            <div className='right'>Test</div>
+            <img className='center' src ={gitLogo} alt="gitLogo"/>
+            </div>
         </footer>
     );
     }
