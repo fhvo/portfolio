3c7e8c1dd0d4b2a854f4422cf1f2d952b676cc21
xwanats <b.wanats@gmail.com>
2022-05-17 22:10:06 +0700

    add tailwind

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header';
 import Footer from './components/Footer';
 import NavBar from './components/NavBar';
 const App = () => {
   return (
     <div className="main">
-        <Header />
         <NavBar/>
-        <h1>Lorem Ipsum</h1>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
         <Footer/>
     </div>
   );
