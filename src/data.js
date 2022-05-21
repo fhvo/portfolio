4194642ecacf1c504a7310f387905fa9dc735238
xwanats <b.wanats@gmail.com>
2022-05-21 18:42:33 +0700

    red

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ import iImg from './img/ice.jpg'
 import dImg from './img/don.jpg'
 
 export const members = [
-  { id: 0, img: {pImg}, link: "https://www.linkedin.com/in/xwanats/" },
+  { id: 0, img: 'https://temp.compsci88.com/manga/Berserk/0364-026.png', link: "https://www.linkedin.com/in/xwanats/" },
   { id: 1, img: {iImg}, link: "https://www.linkedin.com/in/xwanats/" },
   { id: 2, img: {dImg}, link: "https://www.linkedin.com/in/xwanats/" }
 ];
