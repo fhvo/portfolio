9a1127a5a0a0b832b716817022ac5ee6201f9084
xwanats <b.wanats@gmail.com>
2022-05-26 02:45:53 +0700

    add contact with emailsender

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import SloganImg from './img/slogan.jpg';
 import About from './components/about/About';
 import Intro from './components/intro/Intro';
 import MemberList from './components/memberList/MemberList';
+import Contact from './components/contact/Contact';
+
+
 const App = () => {
   return (
     <div className="main">
       <Intro />
       <About image={SloganImg}/>
       <MemberList/>
+      <Contact/>
     </div>
   );
 }
---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import './contact.css';
+
+import Phone from '../../img/phone.png';
+import Email from '../../img/email.png';
+import Address from '../../img/address.png';
+
+import {useRef, useState} from 'react';
+import emailjs from "emailjs-com";
+
+function Contact() {
+  const formRef = useRef();
+  const [done, setDone] = useState(false);
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    emailjs.sendForm(
+      'service_giohdsk',
+      'template_i8z20l8',
+      formRef.current,
+      'gIsMsw2ziBsOuWZrb'
+    ).then((result) => {
+          console.log(result.text);
+          setDone(true);
+      }, (error) => {
+          console.log(error.text);
+      });
+    };
+
+  return (
+    <div className='c'>
+      <div className='c-bg'></div>
+      <div className='c-wrapper'>
+        <div className='c-left'>
+          <h1 className='c-title'>
+            Contact
+          </h1>
+          <div className='c-info'>
+            <div className="c-info-item">
+              <img className='c-icon' src={Phone} alt='' className='c-icon'/>
+              +66 845344xxx
+            </div>
+            <div className="c-info-item">
+              <img className='c-icon' src={Email} alt='' className='c-icon'/>
+              b.wanats@gmail.com
+            </div>
+            <div className="c-info-item">
+              <img className='c-icon' src={Address} alt='' className='c-icon'/>
+              Bangkok, Thailand
+            </div>
+          </div>
+        </div>
+        <div className='c-right'>
+          <div className='c-desc'>
+            <b>What's your story?</b> 
+            <br/>
+            Let us help you build the application that's suitable for your business!
+            <form ref={formRef} onSubmit={handleSubmit}>
+              <input type='text' placeholder='Name' name='user_name' />
+              <input type='text' placeholder='Subject' name='user_subject' />
+              <input type='text' placeholder='Email' name='user_email' />
+              <textarea rows="5" placeholder='Message' name='message' />
+              <button>Submit</button>
+            </form>
+            {done && "Thank you..."}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Contact
